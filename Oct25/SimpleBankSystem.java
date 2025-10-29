10d2635c011fa6fc8b6e381bcf84448b373163d7
Rishav-R03 <rishav042023@gmail.com>
2025-10-29 23:41:58 +0530

    simple bank system

--- Oct25/SimpleBankSystem.java
+++ Oct25/SimpleBankSystem.java
@@ -1,52 +1,30 @@
-class Bank {
-    private final long[] bank;
-    private final int n;
+import java.util.Scanner;
 
-    public Bank(long[] bank) {
-        this.bank = bank;
-        this.n = bank.length;
-    }
+class Main {
+    public static int findBits(int x, int y) {
+        int n = x ^ y;
+        int count = 0;
 
-    private boolean isValid(int acc) {
-        return acc > 0 && acc <= n;
-    }
-
-    public boolean transfer(int acc1, int acc2, int money) {
-        if (!isValid(acc1) || !isValid(acc2) || bank[acc1 - 1] < money) {
-            return false;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
         }
-        bank[acc1 - 1] -= money;
-        bank[acc2 - 1] += money;
-        return true;
+
+        return count;
     }
 
-    public boolean deposit(int acc, long money) {
-        if (!isValid(acc))
-            return false;
-        bank[acc - 1] += money;
-        return true;
-    }
-
-    public boolean withdraw(int acc, int money) {
-        if (!isValid(acc) || bank[acc - 1] < money)
-            return false;
-        bank[acc - 1] -= money;
-        return true;
-    }
-}
-
-public class SimpleBankSystem {
     public static void main(String[] args) {
-        long[] accounts = { 1000, 1200, 3000, 200 };
-        Bank bank1 = new Bank(accounts);
-        int amt = 400;
-        if (bank1.deposit(1, amt)) {
-            System.out.println("Deposited " + amt);
-        }
-        if (bank1.withdraw(4, amt)) {
-            System.out.println("Withdraw successfull ");
-        } else {
-            System.out.println("Insufficient balance or invalid acc id");
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+
+        int y = scanner.nextInt();
+
+        System.out.println(x + " in binary is " + Integer.toBinaryString(x));
+        System.out.println(y + " in binary is " + Integer.toBinaryString(y));
+
+        System.out.println("The total number of bits to be flipped is " + findBits(x, y));
+
+        scanner.close();
     }
 }
